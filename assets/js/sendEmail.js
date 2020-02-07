ade437c0d90b936595d8b4a299aca81e3dab12d6
Dom-888 <spinelladomenico88@gmail.com>
2020-02-07 19:18:30 +0000

    Add the proper arguments in the sendMail function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,8 +1,8 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
-        "from_name": contactForm.name.value,
+    emailjs.send("gmail", "codeinstiute_api_project", { //The first argument is the service name, the second is the template name
+        "from_name": contactForm.name.value, //This is a variable defined by the user via the site's form. It must be positioned in email.js using the {{from_name}} format 
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "project_request": contactForm.projectsummary.value,        
     })
     .then(
         function(response) {
